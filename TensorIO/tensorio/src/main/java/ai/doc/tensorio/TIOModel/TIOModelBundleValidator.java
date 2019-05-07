417a207bb4e73cec8595c41810781100bd989fdc
Rohan <rohan@doc.ai>
2019-05-06 17:05:05 -0700

    JSON schema validator added. Added Integration test for validation of schema and the model.json files for models in the assets folder.

--- TensorIO/tensorio/src/main/java/ai/doc/tensorio/TIOModel/TIOModelBundleValidator.java
+++ TensorIO/tensorio/src/main/java/ai/doc/tensorio/TIOModel/TIOModelBundleValidator.java
@@ -1,7 +1,6 @@
 package ai.doc.tensorio.TIOModel;
 
 import android.content.Context;
-import android.util.Log;
 
 import com.fasterxml.jackson.databind.JsonNode;
 import com.github.fge.jackson.JsonLoader;
