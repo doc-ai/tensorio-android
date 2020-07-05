717ff7cf80f14750f17d9d5ee03a59773408e04c
phildow <phil@phildow.net>
2020-07-04 17:49:12 -0700

    move json parsing to abstract class, needs tests (#14, #15)

---
+++ TensorIO/tensorio/src/test/java/ai/doc/tensorio/TIOModel/TIOModelJSONParsingTest.java
@@ -0,0 +1,14 @@
+package ai.doc.tensorio.TIOModel;
+
+import org.junit.After;
+
+import static org.junit.Assert.*;
+
+// TODO: Write tests (#15)
+
+public class TIOModelJSONParsingTest {
+
+    @After
+    public void tearDown() throws Exception {
+    }
+}
