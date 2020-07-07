a4357ad9e8ca171b99f5272bd118421f23e45738
phildow <phil@phildow.net>
2020-07-07 11:51:08 -0700

    update gradle plugin and update java example to use local dependency

--- JavaExample/app/src/main/java/ai/doc/javaexample/MainActivity.java
+++ JavaExample/app/src/main/java/ai/doc/javaexample/MainActivity.java
@@ -7,25 +7,23 @@ import android.os.Handler;
 import android.os.HandlerThread;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
-import android.widget.Toast;
 
 import java.io.IOException;
 import java.io.InputStream;
-import java.util.AbstractMap;
-import java.util.Arrays;
+import java.util.List;
 import java.util.Map;
-import java.util.PriorityQueue;
 import java.util.Set;
 
-import ai.doc.tensorio.TIOLayerInterface.TIOVectorLayerDescription;
 import ai.doc.tensorio.TIOModel.TIOModel;
 import ai.doc.tensorio.TIOModel.TIOModelBundle;
 import ai.doc.tensorio.TIOModel.TIOModelBundleException;
 import ai.doc.tensorio.TIOModel.TIOModelBundleManager;
 import ai.doc.tensorio.TIOModel.TIOModelException;
-
+import ai.doc.tensorio.TIOUtilities.TIOClassificationHelper;
 
 public class MainActivity extends AppCompatActivity {
+
+    private String TAG = "MainActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -52,34 +50,22 @@ public class MainActivity extends AppCompatActivity {
             mHandlerThread.start();
             Handler mHandler = new Handler(mHandlerThread.getLooper());
 
-
             mHandler.post(() -> {
                 // Run the model on the input
                 float[] result = new float[0];
 
                 try {
-                    result = (float[]) model.runOn(scaled);
+                    Map<String,Object> output = model.runOn(scaled);
+                    Map<String, Float> classification = (Map<String, Float>)output.get("classification");
+                    List<Map.Entry<String, Float>> top5 = TIOClassificationHelper.topN(classification, 5);
+
+                    for (Map.Entry<String, Float> entry : top5) {
+                        Log.i(TAG, entry.getKey() + ":" + entry.getValue());
+                    }
                 } catch (TIOModelException e) {
                     e.printStackTrace();
                 }
-
-                Log.i("result", Arrays.toString(result));
-
-                // Build a PriorityQueue of the predictions
-                PriorityQueue<Map.Entry<Integer, Float>> pq = new PriorityQueue<>(10, (o1, o2) -> (o2.getValue()).compareTo(o1.getValue()));
-                for (int i = 0; i < 1001; i++) {
-                    pq.add(new AbstractMap.SimpleEntry<>(i, result[i]));
-                }
-
-                // Show the 10 most likely predictions
-                String[] labels = ((TIOVectorLayerDescription) model.descriptionOfOutputAtIndex(0)).getLabels();
-                for (int i = 0; i < 10; i++) {
-                    Map.Entry<Integer, Float> e = pq.poll();
-                    Log.i(labels[e.getKey()], "" + e.getValue());
-                }
             });
-
-
         } catch (IOException e) {
             e.printStackTrace();
         } catch (TIOModelBundleException e) {
