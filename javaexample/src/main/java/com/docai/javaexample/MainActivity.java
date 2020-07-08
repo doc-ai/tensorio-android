31459505ea8276955a508c0f2b9d0404022af549
phildow <phil@phildow.net>
2020-07-08 16:30:19 -0700

    rename example app test image

--- javaexample/src/main/java/com/docai/javaexample/MainActivity.java
+++ javaexample/src/main/java/com/docai/javaexample/MainActivity.java
@@ -60,7 +60,7 @@ public class MainActivity extends AppCompatActivity {
 
             // Load the Test Image
 
-            InputStream stream = getAssets().open("picture2.jpg");
+            InputStream stream = getAssets().open("elephant.jpg");
             Bitmap bitmap = BitmapFactory.decodeStream(stream);
             final Bitmap scaled = Bitmap.createScaledBitmap(bitmap, 224, 224, false);
 
